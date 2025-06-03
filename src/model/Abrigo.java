96708cb87a86dbab8f0149c3401122e4f9be0eda
Giovanna Federico <giovannamfederico@gmail.com>
2025-06-03 18:48:50 -0300

    Update SensorHandler.java

--- src/model/Abrigo.java
+++ src/model/Abrigo.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
  */
 public class Abrigo {
 
-    /* Atributos */
+    /* aAtributos */
     private int idAbrigo;
     private String nome;
     private String endereco;
